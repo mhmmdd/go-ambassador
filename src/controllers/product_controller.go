eb1a0cd04021a40023cc5bace5333de226bdb924
mhmmdd <mhmmd.dd@gmail.com>
2021-12-18 09:59:09 +0300

    create channels to clear the cache

--- src/controllers/product_controller.go
+++ src/controllers/product_controller.go
@@ -27,6 +27,9 @@ func CreateProducts(c *fiber.Ctx) error {
 	}
 
 	database.DB.Create(&product)
+
+	go database.ClearCache("products_frontend", "products_backend")
+
 	return c.JSON(product)
 }
 
@@ -50,19 +53,9 @@ func UpdateProduct(c *fiber.Ctx) error {
 
 	database.DB.Model(&product).Updates(&product)
 
-	go deleteCache("products_frontend")
-	go deleteCache("products_backend")
-	//go func(key string) {
-	//	time.Sleep(3 * time.Second)
-	//	database.Cache.Del(context.Background(), key)
-	//}("products_frontend")
+	go database.ClearCache("products_frontend", "products_backend")
 
 	return c.JSON(product)
-}
-
-func deleteCache(key string) {
-	time.Sleep(3 * time.Second)
-	database.Cache.Del(context.Background(), key)
 }
 
 func DeleteProduct(c *fiber.Ctx) error {
@@ -71,6 +64,9 @@ func DeleteProduct(c *fiber.Ctx) error {
 	product := models.Product{}
 	product.Id = uint(id)
 	database.DB.Delete(&product)
+
+	go database.ClearCache("products_frontend", "products_backend")
+
 	return nil
 }
 
--- src/database/redis.go
+++ src/database/redis.go
@@ -1,12 +1,38 @@
 package database
 
-import "github.com/go-redis/redis/v8"
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"time"
+)
 
 var Cache *redis.Client
+var CacheChannel chan string
 
 func SetupRedis() {
 	Cache = redis.NewClient(&redis.Options{
 		Addr: "redis:6379",
 		DB:   0,
 	})
+}
+
+func SetupCacheChannel() {
+	CacheChannel = make(chan string)
+
+	go func(ch chan string) {
+		for {
+			time.Sleep(3 * time.Second)
+
+			key := <-ch
+			Cache.Del(context.Background(), key)
+			fmt.Printf("Cache cleared: %s\n", key)
+		}
+	}(CacheChannel)
+}
+
+func ClearCache(keys ...string) {
+	for _, key := range keys {
+		CacheChannel <- key // channel receives the key
+	}
 }
