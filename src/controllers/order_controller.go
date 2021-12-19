8b410945582e2e2fee4a5bcd6df06cee42782e2d
mhmmdd <mhmmd.dd@gmail.com>
2021-12-19 18:14:22 +0300

    completing orders

--- src/controllers/order_controller.go
+++ src/controllers/order_controller.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"ambassador/src/database"
 	"ambassador/src/models"
+	"context"
+	"github.com/bxcodec/faker/v3"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -62,6 +64,7 @@ func CreateOrder(c *fiber.Ctx) error {
 		Country:         request.Country,
 		City:            request.City,
 		Zip:             request.Zip,
+		TransactionId:   faker.Word(),
 	}
 
 	tx := database.DB.Begin()
@@ -102,4 +105,48 @@ func CreateOrder(c *fiber.Ctx) error {
 
 	}
 	return c.JSON(order)
+}
+
+func CompleteOrder(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	order := models.Order{}
+
+	database.DB.Preload("OrderItems").First(&order, models.Order{
+		TransactionId: data["source"],
+	})
+
+	if order.Id == 0 {
+		c.Status(fiber.StatusNotFound)
+		return c.JSON(fiber.Map{
+			"message": "Order not found",
+		})
+	}
+
+	order.Complete = true
+	database.DB.Save(&order)
+
+	go func(order models.Order) {
+		ambassadorRevenue := 0.0
+		adminRevenue := 0.0
+
+		for _, item := range order.OrderItems {
+			ambassadorRevenue += item.AmbassadorRevenue
+			adminRevenue += item.AdminRevenue
+		}
+
+		user := models.User{}
+		user.Id = order.UserId
+		database.DB.First(&user)
+
+		database.Cache.ZIncrBy(context.Background(), "rankings", ambassadorRevenue, user.Name())
+	}(order)
+
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
 }
